8f082fe53c3606be22bca6bb0ce7efde209e3466
mubarizahmed <mebza@live.com>
2023-11-27 23:46:26 +0100

    fix: react-zxing

--- react-zxing/src/constants.ts
+++
@@ -1,6 +0,0 @@
-export const DEFAULT_CONSTRAINTS: MediaStreamConstraints = {
-  audio: false,
-  video: { facingMode: "environment" },
-};
-
-export const DEFAULT_TIME_BETWEEN_DECODING_ATTEMPTS = 300;
