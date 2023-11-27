2a340a49c08de02fb821fdc719baa32e304a5854
mubarizahmed <mebza@live.com>
2023-11-27 21:10:57 +0100

    add: implement sign out

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,20 @@ import { MdDarkMode } from "react-icons/md";
 import { Toggle } from "@/components/ui/toggle";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { AuthSession } from "@supabase/supabase-js";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { supabase } from "@/supabaseClient";
 
 const Navbar = ({ session }: { session: AuthSession }) => {
+
+
   return (
     <>
       <div className="navbar flex flex-row w-full justify-between items-center p-2 pl-4 pr-4">
@@ -33,9 +45,32 @@ const Navbar = ({ session }: { session: AuthSession }) => {
           >
             <MdDarkMode />
           </Button>
-          <Avatar>
-            <AvatarFallback>{session.user.email?.slice(0,2).toUpperCase()}</AvatarFallback>
-          </Avatar>
+          <Dialog>
+            <DialogTrigger asChild>
+              <button>
+                <Avatar>
+                  <AvatarFallback>
+                    {session.user.email?.slice(0, 2).toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+              </button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Sign Out</DialogTitle>
+                <DialogDescription>
+                  Are you sure you want to sign out? You will need to re-enter your credentials to sign in.
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <Button type="submit" onClick={()=>{
+                  supabase.auth.signOut({scope: "global",}).then(() => {
+                    window.location.reload();
+                  });
+                }}>Sign Out</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
       <Separator />
